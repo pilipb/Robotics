7875575b47559a991d81010ad0cd0b1c01b2560e
pilipb <xq19351@bristol.ac.uk>
2024-01-23 16:48:29 +0000

    Motor Control

---
+++ Labsheet_2/Labsheet_2.ino
@@ -0,0 +1,67 @@
+// Powering motors labsheet
+#include "math.h"
+
+// analogueWrite(255) = 5v, 0 = 0v and PWM in between
+
+# define L_PWM_PIN 10
+# define L_DIR_PIN 16
+# define R_PWM_PIN 9
+# define R_DIR_PIN 15
+
+# define FWD HIGH // Remember notation
+# define REV LOW
+
+int speed = 20;
+boolean dir_forwards = true;
+
+void circle(int size) {
+  // go in a circle radius size
+  analogWrite( L_PWM_PIN, speed*sin(size + 10));
+  analogWrite( R_PWM_PIN, speed*sin(size ));
+
+}
+
+// Runs once.
+void setup() {
+
+  // Set all the motor pins as outputs.
+  // There are 4 pins in total to set.
+  // ...
+  pinMode( L_PWM_PIN, OUTPUT);
+  pinMode( R_PWM_PIN, OUTPUT);
+  pinMode( R_DIR_PIN, OUTPUT);
+  pinMode( L_DIR_PIN, OUTPUT);
+  
+  // Set initial direction (HIGH/LOW)
+  if (dir_forwards = true) {
+    analogWrite( L_DIR_PIN, FWD); // l direction dictates forward
+    analogWrite( R_DIR_PIN, REV);
+  } else {
+    analogWrite( L_DIR_PIN, REV); // l direction dictates forward
+    analogWrite( R_DIR_PIN, FWD);
+  }
+
+  // Initial speed  
+  analogWrite( L_PWM_PIN, speed);
+  analogWrite( R_PWM_PIN, speed);
+
+  // Start serial, send debug text.
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("***RESET***");
+}
+
+// Repeats.
+void loop() {
+
+  // Add code to set the direction of rotation
+  // for the left and right motor here.
+
+//  analogWrite( L_PWM_PIN, speed );
+//  analogWrite( R_PWM_PIN, speed );
+  circle(10);
+
+  // An empty loop can block further uploads.
+  // A small delay to prevent this for now.
+  delay(5);
+}
---
+++ Labsheet_2/motors.h
@@ -0,0 +1,34 @@
+// this #ifndef stops this file
+// from being included mored than
+// once by the compiler. 
+#ifndef _MOTORS_H
+#define _MOTORS_H
+
+
+
+// Class to operate the motor(s).
+class Motors_c {
+  public:
+
+    // Constructor, must exist.
+    Motors_c() {
+
+    } 
+
+    // Use this function to 
+    // initialise the pins and 
+    // state of your motor(s).
+    void initialise() {
+
+    }
+
+    // Write a function to operate
+    // your motor(s)
+    // ...
+
+    
+};
+
+
+
+#endif
