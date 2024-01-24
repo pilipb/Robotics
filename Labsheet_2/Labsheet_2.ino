235a0dd486c3e6c2337d988730be09c27c99fac8
pilipb <xq19351@bristol.ac.uk>
2024-01-24 19:46:43 +0000

    refactored to use class motors

--- Labsheet_2/Labsheet_2.ino
+++ Labsheet_2/Labsheet_2.ino
@@ -1,75 +1,30 @@
 // Powering motors labsheet
 #include "math.h"
+#include "motors.h"
 
-// analogueWrite(255) = 5v, 0 = 0v and PWM in between
+Motors_c motors;
 
-// minimum PWMs
-// forwards = 14
-// backwards = 17
-
-# define L_PWM_PIN 10
-# define L_DIR_PIN 16
-# define R_PWM_PIN 9
-# define R_DIR_PIN 15
-
-# define REV HIGH // Remember notation
-# define FWD LOW
-
-int speed = 45;
-boolean dir_forwards = true;
-unsigned long startTime;
-
-void circle(int size) {
-  // go in a circle radius size
-  analogWrite( L_PWM_PIN, speed * sin(size + 2));
-  analogWrite( R_PWM_PIN, speed * sin(size ));
-
-}
-
-void set_dir(boolean forwards) {
-  if (forwards) {
-    digitalWrite( L_DIR_PIN, FWD);
-    digitalWrite( R_DIR_PIN, FWD);
-  } else {
-    digitalWrite( L_DIR_PIN, REV);
-    digitalWrite( R_DIR_PIN, REV);
-  }
-}
-
-
-// Runs once.
 void setup() {
 
   // Set all the motor pins as outputs.
-  pinMode( L_PWM_PIN, OUTPUT);
-  pinMode( R_PWM_PIN, OUTPUT);
-  pinMode( R_DIR_PIN, OUTPUT);
-  pinMode( L_DIR_PIN, OUTPUT);
-
-  set_dir(dir_forwards);
+  motors.initialise();
 
   // Start serial, send debug text.
   Serial.begin(9600);
-  startTime = millis();
+  Serial.println("**RESET**");
+  delay(100);
 
 }
 
 // Repeats.
 void loop() {
 
-  unsigned long currentTime = millis();
-  unsigned long elapsedTime = currentTime - startTime;
-  if (elapsedTime >= 5000) {
-    Serial.println("Time: " + String(elapsedTime));
-    while (1) {
-      analogWrite( L_PWM_PIN, 0 );
-      analogWrite( R_PWM_PIN, 0 );
-    }
-    return;
-  }
+  motors.setMotorPower(25, 25);
+  delay(5000);
+  motors.setMotorPower(-25, -25);
+  delay(5000);
 
-  analogWrite( L_PWM_PIN, speed );
-  analogWrite( R_PWM_PIN, speed );
+  motors.stop_robot();
 
-  delay(50);
+
 }
