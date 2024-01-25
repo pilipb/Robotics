6d720bc8603d5d6f637165bda6eb86eca7a6a7a9
pilipb <xq19351@bristol.ac.uk>
2024-01-25 11:32:07 +0000

    linesensor class

--- Labsheet_3/Labsheet_3.ino
+++ Labsheet_3/Labsheet_3.ino
@@ -1,61 +1,16 @@
 
-#define EMIT_PIN 11
-#define LS_PIN_1 12 // line sensor 1
-#define LS_PIN_2 A0
-#define LS_PIN_3 A2
-#define LS_PIN_4 A3
-#define LS_PIN_5 A4
+#include "linesensor.h"
 
-int ls_pins[5] = { LS_PIN_1, LS_PIN_2, LS_PIN_3, LS_PIN_4 , LS_PIN_5 };
+LineSensor_c linesensor;
 
 
 #define MAX_SAMPLES 10
 float results[ MAX_SAMPLES ];
 
-float readLineSensor(int sensorNumber) {
-  //  function to read ls 1-5 and output float val
-  //  12, A0, A2, A3, A4
-  if (sensorNumber < 0 || sensorNumber > 4) {
-    // Invalid sensor number, handle error or return a specific value
-    Serial.println("Invalid sensor number");
-    return 0;
-  }
-  int sensorPin = ls_pins[sensorNumber];
-
-  pinMode( EMIT_PIN, OUTPUT ); // if its input uses ambient light
-  digitalWrite( EMIT_PIN, HIGH ); // turns on IR
-
-  pinMode( sensorPin, OUTPUT );
-  digitalWrite( sensorPin, HIGH ); // charges capacitor
-  delayMicroseconds( 10 ); // delay to charge
-
-  unsigned long start_time = micros();
-  pinMode( sensorPin, INPUT );
-  while (digitalRead( sensorPin) == HIGH) {
-    // Do nothing here (waiting).
-    //
-    //    //    time out check:
-    if (micros() - start_time >= 30000) {
-      // time out happens because not enough IR reflecting
-      // (not on surface = 2400 with IR)
-
-      break;
-    }
-
-  }
-  unsigned long end_time = micros();
-
-  pinMode(EMIT_PIN, INPUT);
-
-  unsigned long elapsed_time = end_time - start_time;
-  return (float)elapsed_time;
-}
-
 
 void setup() {
 
-  // Set some initial pin modes and states
-  pinMode( EMIT_PIN, OUTPUT ); // Set EMIT as an input (off)
+  linesensor.initialise();
 
   // Start Serial, wait to connect, print a debug message.
   Serial.begin(9600);
@@ -67,22 +22,22 @@ void setup() {
 
 void loop() {
 
-  float elapsed_time = readLineSensor(3);
+  float elapsed_time = linesensor.readLineSensor(3);
   Serial.println(elapsed_time);
   delay(500);
 
   //
-  for (int i = 0; i < MAX_SAMPLES; i++) {
-    results[i] = readLineSensor(0);
-    delay(200);
-  }
+//  for (int i = 0; i < MAX_SAMPLES; i++) {
+//    results[i] = readLineSensor(0);
+//    delay(200);
+//  }
 
-  while ( true ) {
-    Serial.println("Results: ");
-    for ( int i = 0; i < MAX_SAMPLES; i++ ) {
-      Serial.println( results[i] );
-    }
-    delay(1000);
-  }
+//  while ( true ) {
+//    Serial.println("Results: ");
+//    for ( int i = 0; i < MAX_SAMPLES; i++ ) {
+//      Serial.println( results[i] );
+//    }
+//    delay(1000);
+//  }
 
 }
--- Labsheet_3/linesensor.h
+++ Labsheet_3/linesensor.h
@@ -1,19 +1,72 @@
-// this #ifndef stops this file
-// from being included mored than
-// once by the compiler. 
+
 #ifndef _LINESENSOR_H
 #define _LINESENSOR_H
+
+#define EMIT_PIN 11
+#define LS_PIN_1 12 // line sensor 1
+#define LS_PIN_2 A0
+#define LS_PIN_3 A2
+#define LS_PIN_4 A3
+#define LS_PIN_5 A4
+
+int ls_pins[5] = { LS_PIN_1, LS_PIN_2, LS_PIN_3, LS_PIN_4 , LS_PIN_5 };
 
 // Class to operate the linesensor(s).
 class LineSensor_c {
   public:
-  
+
     // Constructor, must exist.
     LineSensor_c() {
 
-    } 
+    }
 
-   
+    void initialise() {
+      // Set some initial pin modes and states
+      pinMode( EMIT_PIN, OUTPUT ); // Set EMIT as an input (off)
+      digitalWrite( EMIT_PIN, HIGH ); // turns on IR
+    }
+
+
+
+
+    float readLineSensor(int sensorNumber) {
+      //  function to read ls 1-5 and output float val index 0
+      //  12, A0, A2, A3, A4
+      if (sensorNumber < 0 || sensorNumber > 4) {
+        // Invalid sensor number, handle error or return a specific value
+        Serial.println("Invalid sensor number");
+        return 0;
+      }
+      int sensorPin = ls_pins[sensorNumber];
+
+      pinMode( sensorPin, OUTPUT );
+      digitalWrite( sensorPin, HIGH ); // charges capacitor
+      delayMicroseconds( 10 ); // delay to charge
+
+      unsigned long start_time = micros();
+      pinMode( sensorPin, INPUT );
+      while (digitalRead( sensorPin) == HIGH) {
+        // Do nothing here (waiting).
+        //
+        //    //    time out check:
+        if (micros() - start_time >= 30000) {
+          // time out happens because not enough IR reflecting
+          // (not on surface = 2400 with IR)
+
+          break;
+        }
+
+      }
+      unsigned long end_time = micros();
+
+      pinMode(EMIT_PIN, INPUT);
+
+      unsigned long elapsed_time = end_time - start_time;
+      return (float)elapsed_time;
+    }
+
+
+
 
 };
 
