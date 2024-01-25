128f1cfd916c6a36467a2864f921d666d4117989
pilipb <xq19351@bristol.ac.uk>
2024-01-25 11:00:42 +0000

    sensor reading function

--- Labsheet_3/Labsheet_3.ino
+++ Labsheet_3/Labsheet_3.ino
@@ -1,73 +1,82 @@
 
 #define EMIT_PIN 11
-#define LS_LEFT_PIN 12 // line sensor 1
+#define LS_PIN_1 12 // line sensor 1
+#define LS_PIN_2 A0
+#define LS_PIN_3 A2
+#define LS_PIN_4 A3
+#define LS_PIN_5 A4
+
+int ls_pins[5] = { LS_PIN_1, LS_PIN_2, LS_PIN_3, LS_PIN_4 , LS_PIN_5 };
+
 
 #define MAX_SAMPLES 10
 float results[ MAX_SAMPLES ];
+
+float readLineSensor(int sensorNumber) {
+  //  function to read ls 1-5 and output float val
+  //  12, A0, A2, A3, A4
+  if (sensorNumber < 0 || sensorNumber > 4) {
+    // Invalid sensor number, handle error or return a specific value
+    Serial.println("Invalid sensor number");
+    return 0;
+  }
+  int sensorPin = ls_pins[sensorNumber];
+
+  pinMode( EMIT_PIN, OUTPUT ); // if its input uses ambient light
+  digitalWrite( EMIT_PIN, HIGH ); // turns on IR
+
+  pinMode( sensorPin, OUTPUT );
+  digitalWrite( sensorPin, HIGH ); // charges capacitor
+  delayMicroseconds( 10 ); // delay to charge
+
+  unsigned long start_time = micros();
+  pinMode( sensorPin, INPUT );
+  while (digitalRead( sensorPin) == HIGH) {
+    // Do nothing here (waiting).
+    //
+    //    //    time out check:
+    if (micros() - start_time >= 30000) {
+      // time out happens because not enough IR reflecting
+      // (not on surface = 2400 with IR)
+
+      break;
+    }
+
+  }
+  unsigned long end_time = micros();
+
+  pinMode(EMIT_PIN, INPUT);
+
+  unsigned long elapsed_time = end_time - start_time;
+  return (float)elapsed_time;
+}
+
 
 void setup() {
 
   // Set some initial pin modes and states
   pinMode( EMIT_PIN, OUTPUT ); // Set EMIT as an input (off)
-  pinMode( LS_LEFT_PIN, INPUT );    // Set line sensor pin to input
 
   // Start Serial, wait to connect, print a debug message.
   Serial.begin(9600);
   delay(1500);
   Serial.println("***RESET***");
 
-} // End of setup()
+}
 
 
 void loop() {
 
+  float elapsed_time = readLineSensor(3);
+  Serial.println(elapsed_time);
+  delay(500);
 
-  pinMode( EMIT_PIN, OUTPUT ); // if its input uses ambient light
-  digitalWrite( EMIT_PIN, HIGH ); // turns on IR
-
-  pinMode( LS_LEFT_PIN, OUTPUT );
-  digitalWrite( LS_LEFT_PIN, HIGH ); // charges capacitor
-  delayMicroseconds( 10 ); // delay to charge
-
-
-
-  unsigned long start_time = micros();
-  pinMode( LS_LEFT_PIN, INPUT );
-  while (digitalRead( LS_LEFT_PIN) == HIGH) {
-    // Do nothing here (waiting).
-    //
-    //    //    time out check:
-    //    if (micros() - start_time >= 2400) {
-    //          // time out happens because not enough IR reflecting
-    //          // (not on surface = 2400 with IR)
-    //
-    //      break;
-    //    }
-
-  }
-
-  unsigned long end_time = micros();
-
-  pinMode(EMIT_PIN, INPUT);
-
-  unsigned long elapsed_time = end_time - start_time;
-
+  //
   for (int i = 0; i < MAX_SAMPLES; i++) {
-    // This line is complete, but it needs to be
-    // within a for loop construct.  Complete this.
-    results[i] = (float)elapsed_time;
-    delay(1000);
+    results[i] = readLineSensor(0);
+    delay(200);
   }
 
-
-  // The section below has been completed for you.
-  // Use this section to build a similar construct
-  // around the code above, which was Algorithm 1.
-
-  // The robot will be "stuck" here forever, because
-  // the condition true will never change.
-  // Therefore, the robot will Serial Print the
-  // results back to your computer every 1000ms.
   while ( true ) {
     Serial.println("Results: ");
     for ( int i = 0; i < MAX_SAMPLES; i++ ) {
@@ -76,4 +85,4 @@ void loop() {
     delay(1000);
   }
 
-} // End of loop()
+}
