37b29257e5a749d558ba41d4f48a3ffc6d58cef2
pilipb <xq19351@bristol.ac.uk>
2024-01-30 15:27:44 +0000

    replaced linesensor reading with analog read

--- Labsheet_3/Labsheet_3.ino
+++ Labsheet_3/Labsheet_3.ino
@@ -14,7 +14,7 @@ void setup() {
 
 
   linesensor.initialise();
-  motor.initialise();
+//  motor.initialise();
 
   // Configure the Serial port
   Serial.begin(9600);
@@ -23,11 +23,16 @@ void setup() {
 
 void loop() {
 
-  int dir;
-  dir = linesensor.updateDir();
-  motor.turn(dir*20);
-  Serial.print(" dir: " + String(dir) );
-  Serial.print( "\n" );
 
-  delay(50);
+//  int dir;
+//  dir = linesensor.updateDir();
+//  motor.turn(dir * 20);
+//  delay(20);
+//  // go forward
+//  motor.setMotorPower(20,20);
+//  Serial.print(" dir: " + String(dir) );
+//  Serial.print( "\n" );
+  Serial.println("reading: " + String(reading));
+
+  delay(100);
 }
--- Labsheet_3/linesensor.h
+++ Labsheet_3/linesensor.h
@@ -22,8 +22,15 @@ class LineSensor_c {
 
     void initialise() {
       // Set some initial pin modes and states
-      pinMode( EMIT_PIN, INPUT ); // Set EMIT as an input (off)
+      pinMode( EMIT_PIN, OUTPUT ); // Set EMIT as an input (off)
       digitalWrite( EMIT_PIN, HIGH ); // turns on IR
+
+      // set pins as resistor
+      pinMode(ls_pins[0], INPUT_PULLUP);
+      pinMode(ls_pins[1], INPUT_PULLUP);
+      pinMode(ls_pins[2], INPUT_PULLUP);
+      pinMode(ls_pins[3], INPUT_PULLUP);
+      pinMode(ls_pins[4], INPUT_PULLUP);
     }
 
 
@@ -52,54 +59,19 @@ class LineSensor_c {
     boolean onLine(int sensorNumber) {
       // returns whether the given sensor is on the line (true)
       //      or not
-      float val = readLineSensor(sensorNumber);
-      if (val > 1200) {
+      float val = analogLineSensor(sensorNumber);
+      if (val > 600) {
         return true;
       } else {
         return false;
       }
     }
 
-
-    float readLineSensor(int sensorNumber) {
-      //  function to read ls 1-5 and output float val index 0
-      //  12, A0, A2, A3, A4
-      pinMode( EMIT_PIN, OUTPUT );
-      digitalWrite( EMIT_PIN, HIGH );
-
-      if (sensorNumber < 0 || sensorNumber > 4) {
-        // Invalid sensor number, handle error or return a specific value
-        Serial.println("Invalid sensor number");
-        return 0;
-      }
-      int sensorPin = ls_pins[sensorNumber];
-
-      pinMode( sensorPin, OUTPUT );
-      digitalWrite( sensorPin, HIGH ); // charges capacitor
-      delayMicroseconds( 10 ); // delay to charge
-
-      unsigned long start_time = micros();
-      pinMode( sensorPin, INPUT );
-      while (digitalRead( sensorPin) == HIGH) {
-        // Do nothing here (waiting).
-        //
-        //    //    time out check:
-        if (micros() - start_time >= 30000) {
-          // time out happens because not enough IR reflecting
-          // (not on surface = 2400 with IR)
-
-          break;
-        }
-
-      }
-      unsigned long end_time = micros();
-
-      pinMode(EMIT_PIN, INPUT);
-
-      unsigned long elapsed_time = end_time - start_time;
-      return (float)elapsed_time;
+    int analogLineSensor(int sensorNumber) {
+      int val;
+      val = analogRead(sensorNumber);
+      return val;
     }
-
 
 
 
