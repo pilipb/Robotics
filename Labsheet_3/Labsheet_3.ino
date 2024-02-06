17eff7af6eddc792f2a204af877acb3cedcf1eea
pilipb <xq19351@bristol.ac.uk>
2024-02-06 16:58:28 +0000

    implement PID on wheels

--- Labsheet_3/Labsheet_3.ino
+++ Labsheet_3/Labsheet_3.ino
@@ -5,7 +5,8 @@
 #include "kinematics.h"
 #include "pid.h"
 
-PID_c pid;
+PID_c pid0;
+PID_c pid1;
 Motors_c motor;
 LineSensor_c linesensor;
 Kinematics_c matics;
@@ -20,10 +21,11 @@ unsigned long ts;
 // pid
 float feedback0;
 float feedback1;
+float demand;
 
 // rot velocity
-double vel_rot0;
-double vel_rot1;
+float vel_rot0;
+float vel_rot1;
 
 void setup() {
 
@@ -32,13 +34,15 @@ void setup() {
   linesensor.initialise();
   motor.initialise();
   matics.initialise();
-  pid.initialise(5);
+  pid0.initialise(80, 0.95, 0.1);
+  pid1.initialise(80, 0.95, 0.1);
 
   e0_count_t = 0;
   e1_count_t = 0;
 
   feedback0 = 0;
   feedback1 = 0;
+  demand = 1;
 
   ts = millis();
 
@@ -50,28 +54,21 @@ void setup() {
 
 void loop() {
 
-  motor.setMotorPower(10 + feedback0, 10 + feedback1);
+  motor.setMotorPower(feedback1, feedback0);
 
   unsigned long elapsed_ts = millis() - ts;
 
-  if ( elapsed_ts > 10) {
-
+  if ( elapsed_ts > 20) {
 
     vel_rot0 = vel_rot(0, elapsed_ts);
     vel_rot1 = vel_rot(1, elapsed_ts);
 
-    feedback0 = pid.update(5, vel_rot0);
-    feedback1 = pid.update(5, vel_rot0);
+    feedback0 = pid0.update(demand, vel_rot0, elapsed_ts);
+    feedback1 = pid1.update(demand, vel_rot1, elapsed_ts);
 
-    Serial.print("Demand:");
-    Serial.print(5);
-    Serial.print(" ");
-    Serial.print("feedback:");
-    Serial.print(feedback0);
-    Serial.print(" ");
-    Serial.print("measure:");
-    Serial.println(vel_rot0);
-
+    Serial.print(demand);
+    Serial.print(",");
+    Serial.println(vel_rot1);
 
     e0_count_t = count_e0;
     e1_count_t = count_e1;
@@ -79,10 +76,11 @@ void loop() {
   }
 }
 
-double vel_rot(int wheel, unsigned long elapsed_ts) {
+float vel_rot(int wheel, unsigned long elapsed_ts) {
 
-  float alpha = 0.1;
-  float delta_e = 0;
+  //  float alpha = 0.1;
+  float delta_e;
+  float dt = elapsed_ts;
 
   if (wheel == 0) {
     delta_e = count_e0 - e0_count_t;
@@ -93,9 +91,7 @@ double vel_rot(int wheel, unsigned long elapsed_ts) {
     return 0;
   }
 
-  // low pass filter
-  double velocity = delta_e / elapsed_ts;
-//  double fil_velocity = alpha * velocity + (1 - alpha) * velocity;
+  float velocity = delta_e / dt;
 
   return velocity;
 
