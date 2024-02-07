de800642d18b6373f4128bac5912a013349f6ba2
pilipb <xq19351@bristol.ac.uk>
2024-02-07 12:01:56 +0000

    follow line state works and added

--- main/main.ino
+++ main/main.ino
@@ -10,6 +10,28 @@ Motors_c motor;
 LineSensor_c linesensor;
 Kinematics_c matics;
 
+// ls states
+boolean online0;
+boolean online1;
+boolean online2;
+boolean online3;
+boolean online4;
+
+
+// timings
+unsigned long ts;
+
+
+// define states
+#define DEBUG 0
+#define TO_LINE 1
+#define JOIN_LINE 2
+#define FOLLOW_LINE 3
+#define TURN_AROUND 4
+
+
+int state;
+
 void setup() {
 
   setupEncoder0();
@@ -18,11 +40,16 @@ void setup() {
   motor.initialise();
   matics.initialise();
 
+  e0_count_t = 0;
+  e1_count_t = 0;
+
   // start time
   ts = millis();
 
   // Configure the Serial port
   Serial.begin(9600);
+
+  state = FOLLOW_LINE;
 
 
 }
@@ -37,19 +64,48 @@ void loop() {
 
     // identify state and set state
 
+    //    update matics
+    long delta_e0 = count_e0 - e0_count_t;
+    long delta_e1 = count_e1 - e1_count_t;
+    matics.update(delta_e0, delta_e1);
+    e0_count_t = count_e0;
+    e1_count_t = count_e1;
+
+    //    update linesensors
+    online0 = linesensor.onLine(0);
+    online1 = linesensor.onLine(1);
+    online2 = linesensor.onLine(2);
+    online3 = linesensor.onLine(3);
+    online4 = linesensor.onLine(4);
+
+
+
   }
 
   // state actions...
 
-  if (state==DEBUG) {
+  if (state == DEBUG) {
+
+
+
+  } else if (state == TO_LINE) {
+
+
+
+  } else if (state == JOIN_LINE) {
+
+
+    
+  } else if (state == FOLLOW_LINE) {
+
+      float dir = linesensor.weightFollow();
+      motor.stayOnLine(dir, 30);
+
+  } else if (state == TURN_AROUND) {
+
+
     
   }
-
-
-
-
-
-
 
 
 
