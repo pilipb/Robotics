faa972ee10443956d12692f628705b32a68dd5d8
pilipb <xq19351@bristol.ac.uk>
2024-02-12 13:10:22 +0000

    return to home working

--- main/state.h
+++ main/state.h
@@ -28,6 +28,9 @@ int prev_state;
 
 float last_angle;
 
+long dist_x;
+long dist_y;
+
 // Class to contain generic PID algorithm.
 class STATE_c {
   public:
@@ -43,23 +46,29 @@ class STATE_c {
 
     void update(boolean online0, boolean online1, boolean online2, boolean online3, boolean online4, unsigned long start_time) {
 
-      //    update state
+      // first travel to line in straight line until detect line
       if (state == TO_LINE && online0 && online1 && online2 && online3 && online4) {
-
         motor.stop_robot();
         state = JOIN_LINE;
         prev_state = TO_LINE;
 
+        // join line by rotating
       } else if (state == JOIN_LINE && (global_theta > (PI / 6))) {
 
         state = FOLLOW_LINE;
         prev_state = JOIN_LINE;
 
-      } else if ((state == RIGHT_ANGLE_L | state == RIGHT_ANGLE_R ) && (abs(global_theta - last_angle) > (PI / 3))) {
+      } else if ((state == RIGHT_ANGLE_L | state == RIGHT_ANGLE_R ) && (abs(global_theta - last_angle) > (PI / 2))) {
         // safety stop to not miss line
         state = FOLLOW_LINE;
 
-      } else if (state == TURN_AROUND && (abs(global_theta - last_angle) > PI / 2)) {
+        // continuing turning around until pi/2
+        //      } else if (state == TURN_AROUND && (abs(global_theta - last_angle) < PI / 2)) {
+        //
+        //        state = TURN_AROUND;
+        //        prev_state = TURN_AROUND;
+
+      } else if (state == TURN_AROUND && (abs(global_theta - last_angle) > PI)) {
 
         state = FOLLOW_LINE;
         prev_state = TURN_AROUND;
@@ -76,34 +85,51 @@ class STATE_c {
         prev_state = FOLLOW_LINE;
         last_angle = global_theta;
 
-      } else if (state == FOLLOW_LINE && (online0 + online1 + online2 + online3 + online4 == 0)) {
+      } else if (state == FOLLOW_LINE && prev_state == FOLLOW_LINE && (online1 + online2 + online3 == 0)) {
 
-        if ((millis() - start_time) > 20000) {
-          motor.setMotorPower(0, 0);
+        if ((millis() - start_time) > 1000) {
+
           state = RETURN_HOME;
-        } else {
-          motor.stop_robot();
-          state = TURN_AROUND;
-          prev_state = FOLLOW_LINE;
+          dist_x = global_X;
+          dist_y = global_Y;
           last_angle = global_theta;
-        }
 
-      } else if (state == RETURN_HOME) {
+        } else {
 
-        while (true) {
-          motor.stop_robot();
+          state = TURN_AROUND;
+          last_angle = global_theta;
+
         }
 
       }
     }
 
 
-    
 
-    void action() {
+
+    void action(unsigned long elapsed_ts) {
 
       if (state == DEBUG) {
 
+        float ls0 = linesensor.analogLineSensor(0);
+        float ls1 = linesensor.analogLineSensor(1);
+        float ls2 = linesensor.analogLineSensor(2);
+        float ls3 = linesensor.analogLineSensor(3);
+        float ls4 = linesensor.analogLineSensor(4);
+        //        Serial.print(ls0);
+        //        Serial.print(ls1);
+        //        Serial.print(ls2);
+        //        Serial.print(ls3);
+        //        Serial.println(ls4);
+        Serial.print(global_X);
+        Serial.print("X");
+        Serial.print(",");
+        Serial.print(global_Y);
+        Serial.print("Y");
+        Serial.print(",");
+        Serial.print(global_theta);
+        Serial.println("theta");
+//        motor.turn_to(2*PI, elapsed_ts, 12);
 
       } else if (state == TO_LINE) {
 
@@ -130,6 +156,17 @@ class STATE_c {
       } else if (state == RIGHT_ANGLE_L) {
 
         motor.turn_left();
+
+      } else if (state == RETURN_HOME) {
+
+        // calculate distance travelled
+        if ((abs(global_X) + abs(global_Y)) > 50) {
+          
+          motor.turn_to( -PI/2 - (atan(global_X/global_Y)), elapsed_ts, 12);
+
+        } else {
+          motor.stop_robot();
+        }
 
       }
     }
